05543d8341650b86da663581ec576c19c85aa3ab
aayan-shaikh <aayan@aayanshaikh.com>
2025-12-09 15:52:17 +0530

    Home Page V1

---
+++ src/pages/workshops/2025-12-04-testing-new-workshop.md
@@ -0,0 +1,6 @@
+---
+title: Testing New Workshop
+date: 2025-12-04T16:35:00.000+05:30
+img: https://images.pexels.com/photos/34948748/pexels-photo-34948748.jpeg
+---
+Test
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
-import decapCMS from 'vite-plugin-decap-cms';
+
 
 export default defineConfig({
-    plugins: [
-        decapCMS(), // ← does all the noExternal / optimizeDeps magic for you
-        // …your other plugins
-    ],
+
 });
